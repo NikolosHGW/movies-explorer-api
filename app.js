29854042ee3682062b369b9d03d18f63e6687fda
Ник Николос <nic23173@gmail.com>
2021-06-01 11:14:55 +0300

    feat: create app and init mongoose

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use((err, _req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+
+  next();
+});
+
+app.listen(PORT);
